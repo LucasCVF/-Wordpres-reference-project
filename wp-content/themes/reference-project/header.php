bc384fe524336c01badc9ceb00752db948961c33
LucasCVF <lucasferreiracf14@gmail.com>
2024-09-19 14:55:10 -0300

    html header preparation

--- wp-content/themes/reference-project/header.php
+++ wp-content/themes/reference-project/header.php
@@ -1,26 +1,50 @@
-<?php
-/**
- * The header for our theme
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package Teste
- */
-
-?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-
-	<?php wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/header.css" type="text/css"/>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<h1>navbar</h1>
+<header style="height: 81px; min-height: unset;">
+    <nav class="navbar navbar-expand-lg logo-nav">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 p-2">
+                    <a class="navbar-brand-logo" href="">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/fisio_logo.png" alt="Logo">
+                    </a>
+                </div>
+                <div class="col-md-6">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<?php
+						wp_nav_menu(array(
+							'theme_location' => 'menu-header',
+							'container' => false,
+							'menu_class' => 'navbar-nav me-auto mb-2 mb-lg-0',
+							'fallback_cb' => false,
+							'depth' => 2
+						));
+						
+						// Adicione isso para depuração
+						if (!has_nav_menu('menu-header')) {
+							echo '<p>Menu não está definido.</p>';
+						}
+						?>
+					</div>
+                </div>
+                <div class="col-md-3 p-2">
+                    Contato e barra de pesquisa
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
